81a37efa35ef33ee0c383770cec3587813868a79
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-10-11 13:01:37 +0800

    Create build.py to build _rbush

---
+++ build.py
@@ -0,0 +1,20 @@
+from pybind11.setup_helpers import Pybind11Extension
+
+
+def build(setup_kwargs: dict):
+    ext_modules = [
+        Pybind11Extension(
+            "_rbush",
+            sources=["_rbush/module.cc", "_rbush/_rbush.cc"],
+            depends=["_rbush/_rbush.h"],
+            extra_compile_args=["-O3"],
+            language="c++",
+            cxx_std=17,
+        ),
+    ]
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "zip_safe": False,
+        }
+    )
