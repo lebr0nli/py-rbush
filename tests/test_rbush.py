58f5903a4f0f82344314fe37842f9312d98d3639
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-11-16 18:09:16 +0800

    Create tests for `remove` methods

--- tests/test_rbush.py
+++ tests/test_rbush.py
@@ -51,3 +51,64 @@ def test_inheritance():
     all_items = tree.all()
     assert len(all_items) == 1
     assert all_items[0] == item
+
+
+def test_remove_without_fn():
+    tree = rbush.RBush()
+    items = []
+    n = 500
+    for i in range(n):
+        items.append(
+            {
+                "min_x": i,
+                "min_y": i,
+                "max_x": i + 1,
+                "max_y": i + 1,
+            }
+        )
+        tree.insert(items[i])
+
+    assert len(tree.all()) == n
+
+    # remove item from the tree one by one
+    for i in range(n):
+        tree.remove(items[i])
+        all_items = tree.all()
+        assert len(all_items) == n - 1 - i
+        assert all(item in all_items for item in items[i + 1 :])
+        assert items[i] not in all_items
+
+
+def test_remove_with_fn():
+    tree = rbush.RBush()
+    items = []
+    n = 500
+    for i in range(n):
+        items.append(
+            {
+                "min_x": i,
+                "min_y": i,
+                "max_x": i,
+                "max_y": i,
+                "data": i,
+            }
+        )
+        tree.insert(items[i])
+
+    assert len(tree.all()) == n
+
+    # remove item from the tree one by one
+    for i in range(n):
+        # remove without a custom compare function, this should not remove the item
+        tree.remove(items[i].copy())
+        all_items = tree.all()
+        assert len(all_items) == n - i
+        assert all(item in all_items for item in items[i:])
+        assert items[i] in all_items
+
+        # remove with a custom compare function, this should remove the item even is not the same object
+        tree.remove(items[i].copy(), lambda x, y: x["data"] == y["data"])
+        all_items = tree.all()
+        assert len(all_items) == n - 1 - i
+        assert all(item in all_items for item in items[i + 1 :])
+        assert items[i] not in all_items
